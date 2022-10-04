57a260bf4befc53433a2fa0ec770e6e5be3fa472
Riza Fahmi <rizafahmi@gmail.com>
2022-10-04 13:44:17 +0700

    Change production host to localhost

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :kreasidev, KreasidevWeb.Endpoint,
-  url: [host: localhost, port: 4000],
+  url: [host: "localhost", port: 4000],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
