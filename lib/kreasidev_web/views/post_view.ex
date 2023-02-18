07bb08b5c4271598e8ef17750ecd3673d2339a54
Riza Fahmi <rizafahmi@gmail.com>
2023-02-18 22:21:43 +0700

    Add extra info

--- lib/kreasidev_web/views/post_view.ex
+++ lib/kreasidev_web/views/post_view.ex
@@ -1,3 +1,12 @@
 defmodule KreasidevWeb.PostView do
   use KreasidevWeb, :view
+
+  def get_user_info(userid) do
+    user = Kreasidev.Repo.get!(Kreasidev.Users.User, userid)
+    String.split(user.email, "@") |> hd
+  end
+
+  def calc_time(time) do
+    Kreasidev.Timeutil.calc_time(time)
+  end
 end
